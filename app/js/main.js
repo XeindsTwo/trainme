e18953a3f198fd027a74d0088e4360c173f4a298
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-02-10 00:29:28 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,64 @@
+/*const body = document.body;
+const menuBtn = document.querySelector('.menu-btn');
+const header = document.querySelector('.header__mobile');
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+menuBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  body.classList.toggle('body--active');
+  header.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+  menuBtn.blur();
+});
+
+function scrollToTarget(targetId) {
+  const targetSection = document.querySelector(targetId);
+  if (targetSection) {
+    body.classList.remove('body--active');
+    header.classList.remove('active');
+    menuBtn.classList.remove('active');
+    setTimeout(() => {
+      const targetOffset = targetSection.offsetTop;
+      window.scrollTo({top: targetOffset, behavior: 'smooth'});
+    }, 400);
+  }
+}
+
+function handleAnchorClick(event) {
+  event.preventDefault();
+  const targetId = this.getAttribute('href');
+  scrollToTarget(targetId);
+}
+
+for (const anchor of anchors) {
+  anchor.addEventListener('click', handleAnchorClick);
+  anchor.addEventListener('touchstart', handleAnchorClick, {passive: true});
+}
+
+const menuLinks = document.querySelectorAll('.header__link');
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', handleAnchorClick);
+  menuLink.addEventListener('touchstart', handleAnchorClick, {passive: true});
+});*/
+
+const testimonialsBtn = document.getElementById('testimonials-btn');
+const testimonialsOverlay = document.querySelector('.testimonials__overlay');
+const testimonialsControl = document.querySelector('.testimonials__control');
+
+function removeOverlayAndControl() {
+  testimonialsOverlay.remove();
+  testimonialsControl.remove();
+}
+
+function checkIfAlreadyClicked() {
+  return localStorage.getItem('testimonialsClicked');
+}
+
+if (checkIfAlreadyClicked()) {
+  removeOverlayAndControl();
+}
+
+testimonialsBtn.addEventListener('click', function () {
+  removeOverlayAndControl();
+  localStorage.setItem('testimonialsClicked', true);
+});
